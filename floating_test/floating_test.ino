5f3c12c529cae680d37fbc0c425b8fa7a1d82b26
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-17 13:40:59 +0900

    add floating test

--- floating_test/floating_test.ino
+++ floating_test/floating_test.ino
@@ -1,9 +1,24 @@
+
+#define ledPin 13 // 아두이노 내부 LED
+#define swPin 7
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(ledPin, OUTPUT);
+  pinMode(swPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int value = digitalRead(swPin);
+  Serial.println(value);
 
+  if(value == 1) {
+    digitalWrite(ledPin, HIGH);
+    delay(20);
+  } else {
+    digitalWrite(ledPin, LOW);
+    delay(20);
+  }
 }
