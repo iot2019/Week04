bef5bcee48c9f4b01a9b002f44446a3a74b66a51
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-20 23:12:45 +0900

    2019/3/20

--- INPUT_PULL_UP_Serial/INOUT_PULL_UP_Serial.ino
+++ INPUT_PULL_UP_Serial/INOUT_PULL_UP_Serial.ino
@@ -1,10 +1,11 @@
-// PULL UP SWITCH
-// read 값에 따라 내부 LED(13번 ON/OFF
+
+// INPUT_PULLUP SWITCH
+// read 값에 따라 내부 LED(13번) ON/OFF
 int swPin = 4;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(swPin, INPUT);
+  pinMode(swPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
@@ -12,13 +13,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   int value = digitalRead(swPin);
   //Serial.println(value);
-  
+
   if(value == 1) {
-    // 13번 핀의 내부 LED가 OFF이면 ON
+    // 버튼을 누르면 내부 LED(13)가 OFF
     Serial.println(value);
     digitalWrite(13, LOW);
   } else {
-    // 13번 핀의 내부 LED가 ON이면 OFF
+    // 버튼을 때면 내부 LED(13)가 ON
     Serial.println(value);
     digitalWrite(13, HIGH);
   }
